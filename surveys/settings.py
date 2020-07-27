14c38c0b3bd4fb0e6719b6520e1e36610481d8b8
ramazanemreosmanoglu <66941061+ramazanemreosmanoglu@users.noreply.github.com>
2020-07-27 16:21:53 +0300

    Update settings.py

--- surveys/settings.py
+++ surveys/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i%$q+nu5jr8xuvd&)3rm=-hy#65mhr9sv*eys3p@^2k-j-n6t='
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
